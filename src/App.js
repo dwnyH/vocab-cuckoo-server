161df9844577404014d8ac43f335c48a5a3ca753
dawoon <dawoon316@naver.com>
2019-03-05 08:39:35 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -6,20 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="wordBook">cuckoo word book</div>
+        <div className="options">options</div>
+        <div className="alarmOff">today alarm off</div>
       </div>
     );
   }
