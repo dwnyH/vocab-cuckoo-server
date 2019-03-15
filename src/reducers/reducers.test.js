e963cb2166dda80da679fd1d313b05f541a23764
dawoon <dawoon316@naver.com>
2019-03-15 21:40:49 +0900

    Add header component test

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -1,8 +1,8 @@
-import reducer, * as actions from './index';
+import reducer from './index';
 
 describe('reducer', () => {
   let state = reducer(undefined, {});
-  it('should return the initialState', () => {
+  it('should return the initial state', () => {
     expect(state).toHaveProperty('page', 'home');
     expect(state).toHaveProperty('chromeStorageVocabs', []);
     expect(state).toHaveProperty('buttonState', 'log in');
@@ -18,7 +18,7 @@ describe('reducer', () => {
     });
   });
 
-  it('should return modified State', () => {
+  it('should return modified state', () => {
     expect(reducer(state, { type: 'PAGE_TYPE_SEND', pageName: 'home' })).not.toBe(state);
     expect(reducer(state, { type: 'STORAGE_DATA_SEND', storageWords: [] })).not.toBe(state);
     expect(reducer(state, { type: 'ALARM_DATA_SEND', alarmData: {
